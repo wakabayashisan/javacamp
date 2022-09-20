542818b5333d4b477a473fc8377a8ba3840c32cf
wakabayashisan <bloomofthewolf@hotmail.com>
2022-09-20 22:10:31 +0300

    Do-While Döngüsü

---
+++ week1/subject13/Main.java
@@ -0,0 +1,13 @@
+package subject13;
+
+public class Main {
+    public static void main(String[] args) {
+        int j = 33;
+        do {
+            System.out.println(j);
+            j += 3;
+        } while (j < 100);
+        System.out.println("Do-While Döngüsü Bitti");
+
+    }
+}
