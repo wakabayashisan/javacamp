2a61011f02dea898f5009b292fdf9a0dd12d6656
wakabayashisan <bloomofthewolf@hotmail.com>
2022-09-20 21:29:39 +0300

    Temel Veri Tipleri

---
+++ week1/subject7/Main.java
@@ -0,0 +1,12 @@
+package subject7;
+
+public class Main {
+    public static void main(String[] args) {
+        double sayi = 33.3;
+        sayi = -333;
+
+        char karakter = 'U';
+
+        boolean dogruMu=false;
+    }
+}
