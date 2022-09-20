593e718c3f609c2ada6d87e50a4d02940d907605
wakabayashisan <bloomofthewolf@hotmail.com>
2022-09-20 21:52:38 +0300

    For Döngüsü

---
+++ week1/subject11/Main.java
@@ -0,0 +1,11 @@
+package subject11;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=3;i<10;i+=3) {
+            System.out.println(i);
+        }
+            System.out.println("Döngü Bitti");
+    }
+
+}
