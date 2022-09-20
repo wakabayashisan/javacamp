8ed63d98f5a50bb404020d6a17e71c85f4615180
wakabayashisan <bloomofthewolf@hotmail.com>
2022-09-20 21:56:49 +0300

    While Döngüsü

---
+++ week1/subject12/Main.java
@@ -0,0 +1,12 @@
+package subject12;
+
+public class Main {
+    public static void main(String[] args) {
+        int i=3;
+        while(i<77){
+            System.out.println(i);
+            i+=4;
+        }
+        System.out.println("While Döngüsü Bitti");
+    }
+}
