095ee64b4b7a70161ac1d2e56bb5d624cc6ecdee
wakabayashisan <bloomofthewolf@hotmail.com>
2022-09-20 23:27:26 +0300

    Mini Proje

---
+++ week1/subject19/Main.java
@@ -0,0 +1,29 @@
+package subject19;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 3;
+        int remainder = number % 3;
+
+        boolean isPrime = true;
+
+        if (number == 1){
+            System.out.println("Sayı asal değildir.");
+        return;
+    }
+
+        if (number<1){
+            System.out.println("Geçersiz sayı.");
+        }
+            for (int i=2; i<number;i++){
+                if (number % 1 == 0){
+                    isPrime = false;
+                }
+            }
+        if (isPrime) {
+            System.out.println("Sayı asaldır.");
+        }else{
+            System.out.println("Sayı asal değildir.");
+        }
+    }
+}
