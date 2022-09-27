4e66670ec2f3abf522abbf2977bf59eb5d0c08e5
wakabayashisan <bloomofthewolf@hotmail.com>
2022-09-27 22:29:18 +0300

    Classlar

---
+++ week2/subject27and28/Main.java
@@ -0,0 +1,12 @@
+package subject27and28;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager();
+        CustomerManager customerManager2 = new CustomerManager();
+        customerManager = customerManager2;
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+    }
+}
