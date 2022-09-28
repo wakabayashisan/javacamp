49d555cad537ac9f339a1d6ac3a419474b4ee990
wakabayashisan <bloomofthewolf@hotmail.com>
2022-09-28 20:14:12 +0300

    ReCap Demo

---
+++ week2/subject30/Main.java
@@ -0,0 +1,10 @@
+package subject30;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(33, 3);
+        System.out.println(sonuc);
+    }
+}
