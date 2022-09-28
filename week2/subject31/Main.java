7e3628ec873b92a5c1e41c9e43185ab8fc08ded8
wakabayashisan <bloomofthewolf@hotmail.com>
2022-09-28 20:29:08 +0300

    Field ve Attribute

---
+++ week2/subject31/Main.java
@@ -0,0 +1,16 @@
+package subject31;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.name = "Laptop";
+        product.id = 1;
+        product.description = "Toshiba Laptop";
+        product.price = 5000;
+        product.stockAmount = 3;
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product);
+    }
+}
---
+++ week2/subject31/ProductManager.java
@@ -0,0 +1,7 @@
+package subject31;
+
+public class ProductManager {
+    public void Add(Product product){
+        System.out.println("Ürün eklendi.");
+    }
+}
